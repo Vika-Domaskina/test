9d4f7666eacba7bd7b64cc614cc95061ca19341a
Vika-Domaskina <v1ktir1ya.domask1na@gmail.com>
2016-01-19 20:46:09 +0200

    add test case for checking button "like"

--- selentest7_1.py
+++ selentest7_1.py
@@ -1,10 +1,11 @@
-import unittest, time
+import unittest, time, sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC #
 from selenium.webdriver.common.by import By
 import configparser
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 
@@ -66,7 +67,7 @@ class VkTest2(unittest.TestCase):
         driver.wait_by_xpath("//div[@id='im_bar']")
         driver.driver.find_element_by_xpath("//div[@id='im_bar']")
         el=driver.click_by_xpath("//div[@id='im_filter_out']")
-        el.send_keys(config['Vk.com']['user_name']) # find user to write a message
+        el.send_keys(config['Vk.com']['user_name2']) # find user to write a message
         driver.wait_by_xpath("//div[@id='im_friends']")
         
         driver.click_by_xpath("//div[@id='im_friends']/div[position()=1]") # select user
@@ -96,7 +97,63 @@ class VkTest2(unittest.TestCase):
         time.sleep(20)
         
         
+    def test2_vk_like(self):
+    
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+
         
+        driver = self.driver
+        driver2 = self.driver2
+        
+        driver.login_vk(config['Vk.com']['login1'],config['Vk.com']['paswd1'])
+        driver2.login_vk(config['Vk.com']['login2'],config['Vk.com']['paswd2'])
+        time.sleep(3)
+        
+        driver.click_by_xpath("//div[@id='side_bar']/ol/li[@id='l_fr']")
+        driver.wait_by_xpath("//div[@id='friends_search']//input[@id='s_search']")
+        elem=driver.click_by_xpath("//div[@id='friends_search']//input[@id='s_search']")
+        
+        elem.send_keys(config['Vk.com']['user_name2'])  # friends_search
+        driver.click_by_xpath("//div[@id='friends_search']/button[@id='invite_button']")
+        
+        driver.click_by_xpath("//div[@id='list_content']/div/div[position()=1]/div[@class='info fl_l']/div[position()=1]") #friend page
+        
+        driver.wait_by_xpath("//div[@id='page_body']/div[@id='wrap3']//div[@id='page_avatar']/a[@id='profile_photo_link']")
+        driver.click_by_xpath("//div[@id='page_body']/div[@id='wrap3']//div[@id='page_avatar']/a[@id='profile_photo_link']") #friend avatar 
+        
+        driver.wait_by_xpath("//div[@id='pv_wide']/div[@id='pv_like_wrap']/span[@id='pv_like_link']")
+        driver.driver.execute_script("window.scrollTo(0, 150)")
+        driver.click_by_xpath("//div[@id='pv_wide']/div[@id='pv_like_wrap']/span[@id='pv_like_link']") #like friends foto
+        
+        action=webdriver.ActionChains(driver.driver) # move to list 
+        
+        batton_like=WebDriverWait(driver.driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//div[@id='pv_wide']/div[@id='pv_like_wrap']/span[@id='pv_like_link']")))
+        action.move_to_element(batton_like).perform()
+        
+        list_likes=WebDriverWait(driver.driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//div[@class='like_head_wrap']/span[position()=1]")))
+        action.move_to_element(batton_like).click(list_likes).perform()
+        
+        driver.wait_by_xpath("//div[@id='wk_likes_content']/div[@id='wk_likes_rows']/div/div[@class='wk_likes_liker_name']/a")
+        find_like_users=driver.driver.find_elements_by_xpath("//div[@id='wk_likes_content']/div[@id='wk_likes_rows']/div/div[@class='wk_likes_liker_name']/a")
+        name_user1=config['Vk.com']['user_name1']
+        for i in find_like_users:
+            name=i.text
+            if name == name_user1:
+                print('This user:',name,'liked foto')
+                break
+        else:
+            raise ValueError ('Not found user who liked foto')
+       
+        driver.wait_by_xpath("//div[@id='wk_box']/a[@id='wk_close_link']")
+        driver.click_by_xpath("//div[@id='wk_box']/a[@id='wk_close_link']")
+        driver.wait_by_xpath("//div[@id='pv_wide']/div[@id='pv_like_wrap']/span[@id='pv_like_link']") #dislike foto 
+        driver.driver.execute_script("window.scrollTo(0, 150)")
+        driver.click_by_xpath("//div[@id='pv_wide']/div[@id='pv_like_wrap']/span[@id='pv_like_link']")
+        time.sleep(10)
+        
+        
+       
     def tearDown(self):
         print("tearDown")
         self.driver.driver.close()
